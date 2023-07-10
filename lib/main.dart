665117f1b8e38d5781d3fbbc240eba5fed8979e8
Alfred Wemali <alfredwemali05@gmail.com>
2023-07-10 07:48:02 +0300

    adding cross screen navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/categories.dart';
-import 'package:meals/data/dummy_data.dart';
-import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -25,7 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const MealsScreen(title: 'Some Category', meals: dummyMeals)
+      home: const CategoriesScreen()
     );
   }
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: 'Some Title', meals: []),
+      ),//Navigator.push(context, route)
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +26,16 @@ class CategoriesScreen extends StatelessWidget {
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
-            mainAxisSpacing: 20
-          ),
+            mainAxisSpacing: 20),
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            )
         ],
       ),
     );
