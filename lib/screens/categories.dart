ea096028070f3074ff2df06367a088e28fd5f541
Alfred Wemali <alfredwemali05@gmail.com>
2023-07-11 20:04:06 +0300

    adding tab based navigation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -18,11 +18,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your Category'),
-      ),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
@@ -39,7 +35,6 @@ class CategoriesScreen extends StatelessWidget {
               },
             )
         ],
-      ),
-    );
+      );
   }
 }
