06a440c68e9b9c5e935c0151eeb900b8f217ffde
Alfred Wemali <alfredwemali05@gmail.com>
2023-07-11 18:47:12 +0300

    navigation to meals screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -8,16 +9,29 @@ class MealsScreen extends StatelessWidget {
   final String title;
   final List<Meal> meals;
 
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text('Uh oo ... Nothing here!', style: Theme.of(context)
+          Text(
+            'Uh oo ... Nothing here!',
+            style: Theme.of(context)
                 .textTheme
                 .headlineLarge!
-                .copyWith(color: Theme.of(context).colorScheme.onBackground),),
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
           const SizedBox(
             height: 16,
           ),
@@ -35,7 +49,12 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (context, index) => MealItem(meal: meals[index]),
+        itemBuilder: (context, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (meal) {
+            selectMeal(context, meal);
+          },
+        ),
       );
     }
 
